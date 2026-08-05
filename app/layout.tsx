c76879d3eda1e77fb6b63e4799d26e63054281b8
johanna bakouka <johannabakouka@airdejohanna2.home>
2026-08-05 20:45:02 +0200

    fix apple app title apostrophe

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
-    title: "SnappinBuddy",
+    title: "Snappin\u2019Buddy",
   },
   icons: {
     apple: "/logo.png",
@@ -42,7 +42,7 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="SnappinBuddy" />
+        <meta name="apple-mobile-web-app-title" content="Snappin'Buddy" />
         <link rel="apple-touch-icon" href="/logo.png" />
         <link rel="manifest" href="/manifest.json" />
         <script dangerouslySetInnerHTML={{ __html: `
